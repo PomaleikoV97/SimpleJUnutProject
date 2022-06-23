f8a8c1c781b5484b04cc6406be7c14a744218a1f
st <pomalejko.vova@gmail.com>
2022-06-23 22:16:30 +0300

    Add ArrayWorker.changeSign unit-test.

---
+++ src/by/itstep/pomaleiko/javalesson/lesson20/model/logic/ArrayWorker.java
@@ -0,0 +1,28 @@
+package by.itstep.pomaleiko.javalesson.lesson20.model.logic;
+
+public class ArrayWorker {
+    public static int countNegativeValue(int[] array) {
+        if (array == null) {
+            return -1;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void changeSign(int[] array) {
+        if (array.length == 0 || array == null) {
+            throw new RuntimeException();
+        }
+        for (int i = 0; i < array.length; i++) {
+            array[i] *= -1;
+        }
+        return;
+    }
+}
---
+++ test/by/itstep/pomaleiko/javalesson/lesson20/model/logic/ArrayWorkerTest.java
@@ -0,0 +1,136 @@
+package by.itstep.pomaleiko.javalesson.lesson20.model.logic;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ArrayWorkerTest {
+    @Test
+    public void testCountNegativeValuePositive() {
+        int[] array = {1, -2, 3, -4, 5, -6};
+        int expected = 3;
+
+        int actual = ArrayWorker.countNegativeValue(array);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCountNegativeValueWithAllNegativeElements() {
+        int[] array = {-1, -2, -3, -4, -5, -6};
+        int expected = array.length;
+
+        int actual = ArrayWorker.countNegativeValue(array);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCountNegativeValueWithAllPositiveElements() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+        int expected = 0;
+
+        int actual = ArrayWorker.countNegativeValue(array);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCountNegativeValueWithEmptyObject() {
+        int[] array = {};   // int[] array = new int[0];
+        int expected = 0;
+
+        int actual = ArrayWorker.countNegativeValue(array);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testCountNegativeValueWithNull() {
+        int expected = -1;
+
+        int actual = ArrayWorker.countNegativeValue(null);
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void testChangeSignPositive() {
+        int[] array = {1, -2, 3, 0, -4, 5};
+        int[] expected = {-1, 2, -3, 0, 4, -5};
+
+        ArrayWorker.changeSign(array);
+
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] != expected[i]) {
+//                fail();
+//            }
+//        }
+
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testChangeSignWithAllPositiveValues() {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] expected = {-1, -2, -3, -4, -5};
+
+        ArrayWorker.changeSign(array);
+
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testChangeSignWithAllNegativeValues() {
+        int[] array = {-1, -2, -3, -4, -5};
+        int[] expected = {1, 2, 3, 4, 5};
+
+        ArrayWorker.changeSign(array);
+
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testChangeSignWithOneNegativeValues() {
+        int[] array = {-1};
+        int[] expected = {1};
+
+        ArrayWorker.changeSign(array);
+
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testChangeSignWithOnePositiveValues() {
+        int[] array = {1};
+        int[] expected = {-1};
+
+        ArrayWorker.changeSign(array);
+
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testChangeSignWithZeroValues() {
+        int[] array = {0};
+        int[] expected = {0};
+
+        ArrayWorker.changeSign(array);
+
+        assertArrayEquals(expected, array);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testChangeSignWithEmptyObject() {
+        ArrayWorker.changeSign(null);
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testChangeSignWithNull() {
+        ArrayWorker.changeSign(null);
+    }
+}
